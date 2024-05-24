cc78ee416fe8180c0444323f0ac1cf9308e41a4e
fabianscharfetter69 <117461947+fabianscharfetter69@users.noreply.github.com>
2024-05-24 10:25:39 +0200

    Css Update

--- Geburtstagsliste/MainWindow.xaml.cs
+++ Geburtstagsliste/MainWindow.xaml.cs
@@ -13,13 +13,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Geburtstagsliste
 {
